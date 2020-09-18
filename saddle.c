f8173415b51c85fd825d0466ef0f11d87b576318
Soorajm100 <soorajm825@gmail.com>
2020-09-19 01:06:53 +0530

    Update saddle.c

--- saddle.c
+++ saddle.c
@@ -1,60 +1,61 @@
 
+
+
 #include <stdio.h>
-     
-      void saddle(int m[10][10],int n)
-     {
-          
-      int i, j, k, min, max, pos[2][2];
-     
-        for (i = 0; i < n; i++) {
-                min = m[i][0];
-                for (j = 0; j < n; j++) {
-                        if (min >= m[i][j]) {
-                                min = m[i][j];
-                                pos[0][0] = i;
-                                pos[0][1] = j;
-                        }
-                }
 
-                j = pos[0][1];
-                max = m[0][j];
-                for (k = 0; k < n; k++) {
-                        if (max <= m[k][j]) {
-                                max = m[i][j];
-                                pos[1][0] = k;
-                                pos[1][1] = j;
-                        }
-                }
+#include<stdio.h>
 
-               
-                if (min == max) {
-                        if (pos[0][0] == pos[1][0] &&
-                                pos[0][1] == pos[1][1]) {
-                                printf("Saddle point  at the location of (%d, %d)  is : %d\n",
-                                                pos[0][0], pos[0][1], max);
-                        }
-                }
-        }
-      
-  }
+int main() {
 
+   int a[ 10][10], i, j, n,m;
+   int sm, p, large, f = 1;
 
-  int main() {
-        int i, j, m[10][10],n;
+   printf("Enter size of row of matrix:");
+   scanf("%d", & m);
+   
+   printf("Enter size of row of matrix:");
+   scanf("%d", & n);
 
-     
-        printf("enter the row of  the square matrix:");
-        scanf("%d", &n);
+   printf("Enter 2D array elements:");
+   for (i = 0; i < m; i++) {
+       for (j = 0; j < n; j++) {
+           scanf("%d", & a[i][j]);
+       }
+   }
 
-        
-        printf(" enter the elements of the square matrix:\n");
-        for (i = 0; i < n; i++) {
-                for (j = 0; j < n; j++) {
-                        scanf("%d", &m[i][j]);
-                }
-        }
-        printf("********************************************************************\n\n");
-        saddle(m,n);
-          return 0;
-  }
-  
+   printf("Elements of the 2D array is:");
+   for (i = 0; i < m; i++) {
+       printf("\n");
+       for (j = 0; j < n; j++) {
+           printf("%d ", a[i][j]);
+       }
+   }
+
+   /* Logic start here */
+   for (i = 0; i < m; i++) {
+       p = 0;
+       sm = a[i][0];
+       for (j = 0; j < n; j++) {
+           if (sm > a[i][j]) {
+               sm = a[i][j];
+               p = j;
+           }
+       }
+
+       large = 0;
+       for (j = 0; j < m; j++) {
+           if (large < a[j][p]) {
+               large = a[j][p];
+           }
+       }
+       if (sm == large) {
+           printf("\nValue of saddle point:%d", sm);
+           f = 0;
+       }
+   }
+
+   if (f > 0)
+       printf("\nNo saddle point");
+
+   return 0;
+}
